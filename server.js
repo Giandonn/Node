24457fa9c55f93dd04c75e381daf3e1413e25fa1
Giandonn <lucas_raineri04@hotmail.com>
2024-08-03 09:37:33 -0300

    rotas

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ import http from "http";
 
 const PORT = 3000;
 
+const rotas = {
+  "/": "Curso de node",
+  "/livros": "rota de livros",
+  "/autores": "rota de autores"
+};
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Curso de Node");
+  res.end(rotas[req.url]);
 });
 
 server.listen(PORT, () => {
